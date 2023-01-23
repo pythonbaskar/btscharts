c9415598f341690f052869b9a9f24d97ab461694
pythonbaskar <pythonbaskar@gmail.com>
2023-01-23 12:58:22 +0530

    Added by Baskar

---
+++ app.py
@@ -0,0 +1,12 @@
+
+
+
+def mainFunc():
+    print('Hi')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    mainFunc()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
